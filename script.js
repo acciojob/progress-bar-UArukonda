f6a6838342d0d8fd922c0078252a43ab708fde4a
Upender Arukonda <109522168+UArukonda@users.noreply.github.com>
2026-09-11 15:48:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll(".circle");
+
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+
+const progress = document.getElementById("progress");
+
+let currentStep = 1;
+
+next.addEventListener("click", () => {
+
+  if (currentStep < circles.length) {
+    currentStep++;
+
+    circles[currentStep - 1].classList.add("active");
+
+    progress.style.width =
+      `${((currentStep - 1) / (circles.length - 1)) * 100}%`;
+  }
+
+  updateButtons();
+});
+
+
+prev.addEventListener("click", () => {
+
+  if (currentStep > 1) {
+    circles[currentStep - 1].classList.remove("active");
+
+    currentStep--;
+
+    progress.style.width =
+      `${((currentStep - 1) / (circles.length - 1)) * 100}%`;
+  }
+
+  updateButtons();
+});
+
+
+function updateButtons() {
+
+  prev.disabled = currentStep === 1;
+
+  next.disabled = currentStep === circles.length;
+
+}
